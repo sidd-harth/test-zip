c27e416b38a99135dd86a493db7f8beae0dea616
Siddharth Barahalikar <barahalikar.siddharth@gmail.com>
2018-02-21 00:16:58 +0530

    2

--- src/main/java/com/example/demoopentracing/HelloController.java
+++ src/main/java/com/example/demoopentracing/HelloController.java
@@ -22,7 +22,7 @@ public class HelloController {
 
     @RequestMapping("/chaining")
     public String chaining() {
-        ResponseEntity<String> response = restTemplate.getForEntity("http://helloxd/hello", String.class);
+        ResponseEntity<String> response = restTemplate.getForEntity("http://hellozz/hello", String.class);
         return "Chaining + " + response.getBody();
     }
 }
